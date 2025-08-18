379429980b7eb61ccb64c43f112501fb016d38cf
SaiVikrantG <gsvikrant8@gmail.com>
2025-08-18 23:20:29 +0530

    Made changes to database table Users.

--- AirlineProject.sql
+++ AirlineProject.sql
@@ -8,13 +8,27 @@ CREATE TABLE Users (
     role VARCHAR2(10) CHECK (role IN ('admin', 'staff')) NOT NULL
 );
 
-CREATE OR REPLACE TRIGGER trg_users_bi
-BEFORE INSERT ON Users
-FOR EACH ROW
-BEGIN
-    :NEW.user_id := users_seq.NEXTVAL;
-END;
-/
+
+-- altering the constraint of data in roles column
+SELECT constraint_name, search_condition
+FROM user_constraints
+WHERE table_name = 'USERS'
+  AND constraint_type = 'C';
+
+-- select the constraint_name from above query result and pase below in place of query name
+ALTER TABLE Users DROP CONSTRAINT SYS_C007567;
+
+ALTER TABLE Users
+ADD CONSTRAINT ROLE_CHECK
+CHECK (role IN ('ROLE_ADMIN', 'ROLE_STAFF'));
+
+-- CREATE OR REPLACE TRIGGER trg_users_bi
+-- BEFORE INSERT ON Users
+-- FOR EACH ROW
+-- BEGIN
+--     :NEW.user_id := users_seq.NEXTVAL;
+-- END;
+-- /
 
 -- FLIGHTS TABLE
 CREATE SEQUENCE flights_seq START WITH 1 INCREMENT BY 1;
@@ -27,13 +41,13 @@ CREATE TABLE Flights (
     arrival_time TIMESTAMP
 );
 
-CREATE OR REPLACE TRIGGER trg_flights_bi
-BEFORE INSERT ON Flights
-FOR EACH ROW
-BEGIN
-    :NEW.flight_id := flights_seq.NEXTVAL;
-END;
-/
+-- CREATE OR REPLACE TRIGGER trg_flights_bi
+-- BEFORE INSERT ON Flights
+-- FOR EACH ROW
+-- BEGIN
+--     :NEW.flight_id := flights_seq.NEXTVAL;
+-- END;
+-- /
 
 -- PASSENGERS TABLE
 CREATE SEQUENCE passengers_seq START WITH 1 INCREMENT BY 1;
@@ -51,13 +65,13 @@ CREATE TABLE Passengers (
     CONSTRAINT fk_passenger_flight FOREIGN KEY (flight_id) REFERENCES Flights(flight_id) ON DELETE CASCADE
 );
 
-CREATE OR REPLACE TRIGGER trg_passengers_bi
-BEFORE INSERT ON Passengers
-FOR EACH ROW
-BEGIN
-    :NEW.passenger_id := passengers_seq.NEXTVAL;
-END;
-/
+-- CREATE OR REPLACE TRIGGER trg_passengers_bi
+-- BEFORE INSERT ON Passengers
+-- FOR EACH ROW
+-- BEGIN
+--     :NEW.passenger_id := passengers_seq.NEXTVAL;
+-- END;
+-- /
 
 -- SERVICE CATEGORIES TABLE
 CREATE SEQUENCE service_categories_seq START WITH 1 INCREMENT BY 1;
@@ -67,13 +81,13 @@ CREATE TABLE ServiceCategories (
     category_name VARCHAR2(20) CHECK (category_name IN ('ancillary', 'meal', 'shopping')) UNIQUE NOT NULL
 );
 
-CREATE OR REPLACE TRIGGER trg_service_categories_bi
-BEFORE INSERT ON ServiceCategories
-FOR EACH ROW
-BEGIN
-    :NEW.category_id := service_categories_seq.NEXTVAL;
-END;
-/
+-- CREATE OR REPLACE TRIGGER trg_service_categories_bi
+-- BEFORE INSERT ON ServiceCategories
+-- FOR EACH ROW
+-- BEGIN
+--     :NEW.category_id := service_categories_seq.NEXTVAL;
+-- END;
+-- /
 
 -- FLIGHT SERVICES TABLE
 CREATE SEQUENCE flight_services_seq START WITH 1 INCREMENT BY 1;
@@ -89,13 +103,13 @@ CREATE TABLE FlightServices (
     CONSTRAINT fk_service_category FOREIGN KEY (category_id) REFERENCES ServiceCategories(category_id)
 );
 
-CREATE OR REPLACE TRIGGER trg_flight_services_bi
-BEFORE INSERT ON FlightServices
-FOR EACH ROW
-BEGIN
-    :NEW.service_id := flight_services_seq.NEXTVAL;
-END;
-/
+-- CREATE OR REPLACE TRIGGER trg_flight_services_bi
+-- BEFORE INSERT ON FlightServices
+-- FOR EACH ROW
+-- BEGIN
+--     :NEW.service_id := flight_services_seq.NEXTVAL;
+-- END;
+-- /
 
 -- SEAT ASSIGNMENTS TABLE
 CREATE SEQUENCE seat_assignments_seq START WITH 1 INCREMENT BY 1;
@@ -112,13 +126,13 @@ CREATE TABLE SeatAssignments (
     CONSTRAINT uq_passenger_flight UNIQUE (passenger_id, flight_id)
 );
 
-CREATE OR REPLACE TRIGGER trg_seat_assignments_bi
-BEFORE INSERT ON SeatAssignments
-FOR EACH ROW
-BEGIN
-    :NEW.assignment_id := seat_assignments_seq.NEXTVAL;
-END;
-/
+-- CREATE OR REPLACE TRIGGER trg_seat_assignments_bi
+-- BEFORE INSERT ON SeatAssignments
+-- FOR EACH ROW
+-- BEGIN
+--     :NEW.assignment_id := seat_assignments_seq.NEXTVAL;
+-- END;
+-- /
 
 -- PASSENGER ANCILLARY SERVICES
 CREATE SEQUENCE pas_ancillary_seq START WITH 1 INCREMENT BY 1;
@@ -132,13 +146,13 @@ CREATE TABLE PassengerAncillaryServices (
     CONSTRAINT uq_pas_ancillary UNIQUE (passenger_id, service_id)
 );
 
-CREATE OR REPLACE TRIGGER trg_pas_ancillary_bi
-BEFORE INSERT ON PassengerAncillaryServices
-FOR EACH ROW
-BEGIN
-    :NEW.id := pas_ancillary_seq.NEXTVAL;
-END;
-/
+-- CREATE OR REPLACE TRIGGER trg_pas_ancillary_bi
+-- BEFORE INSERT ON PassengerAncillaryServices
+-- FOR EACH ROW
+-- BEGIN
+--     :NEW.id := pas_ancillary_seq.NEXTVAL;
+-- END;
+-- /
 
 -- PASSENGER MEALS
 CREATE SEQUENCE passenger_meals_seq START WITH 1 INCREMENT BY 1;
@@ -152,13 +166,13 @@ CREATE TABLE PassengerMeals (
     CONSTRAINT uq_pas_meal UNIQUE (passenger_id, service_id)
 );
 
-CREATE OR REPLACE TRIGGER trg_passenger_meals_bi
-BEFORE INSERT ON PassengerMeals
-FOR EACH ROW
-BEGIN
-    :NEW.id := passenger_meals_seq.NEXTVAL;
-END;
-/
+-- CREATE OR REPLACE TRIGGER trg_passenger_meals_bi
+-- BEFORE INSERT ON PassengerMeals
+-- FOR EACH ROW
+-- BEGIN
+--     :NEW.id := passenger_meals_seq.NEXTVAL;
+-- END;
+-- /
 
 -- PASSENGER SHOPPING ITEMS
 CREATE SEQUENCE passenger_shopping_seq START WITH 1 INCREMENT BY 1;
@@ -172,10 +186,10 @@ CREATE TABLE PassengerShoppingItems (
     CONSTRAINT uq_pas_shop UNIQUE (passenger_id, service_id)
 );
 
-CREATE OR REPLACE TRIGGER trg_passenger_shopping_bi
-BEFORE INSERT ON PassengerShoppingItems
-FOR EACH ROW
-BEGIN
-    :NEW.id := passenger_shopping_seq.NEXTVAL;
-END;
-/
+-- CREATE OR REPLACE TRIGGER trg_passenger_shopping_bi
+-- BEFORE INSERT ON PassengerShoppingItems
+-- FOR EACH ROW
+-- BEGIN
+--     :NEW.id := passenger_shopping_seq.NEXTVAL;
+-- END;
+-- /
