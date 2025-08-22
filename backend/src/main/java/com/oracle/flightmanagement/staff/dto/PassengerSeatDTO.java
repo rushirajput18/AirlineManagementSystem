c2af98065ab8964a8218f8da586a2369bfbb1a10
sgupt1802 <sgupta_b21@it.vjti.ac.in>
2025-08-22 10:43:43 +0530

    Fetching all details of passenger in staff

--- backend/src/main/java/com/oracle/flightmanagement/staff/dto/PassengerSeatDTO.java
+++ backend/src/main/java/com/oracle/flightmanagement/staff/dto/PassengerSeatDTO.java
@@ -1,20 +1,31 @@
 package com.oracle.flightmanagement.staff.dto;
 
+import java.time.LocalDate;
+
 public class PassengerSeatDTO {
 
     private Long passengerId;
     private String seatNumber;
     private String seatClass;
     private Boolean isCheckedIn;
+    private String name;
+    private LocalDate dateOfBirth;
+    private String passport;
+    private String address;
 
     public PassengerSeatDTO() {
     }
 
-    public PassengerSeatDTO(Long passengerId, String seatNumber, String seatClass, Boolean isCheckedIn) {
+    public PassengerSeatDTO(Long passengerId, String seatNumber, String seatClass, Boolean isCheckedIn, String name, LocalDate dateOfBirth, String passport, String address) {
         this.passengerId = passengerId;
         this.seatNumber = seatNumber;
         this.seatClass = seatClass;
         this.isCheckedIn = isCheckedIn;
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.passport = passport;
+        this.address = address;
+
     }
 
     public Long getPassengerId() {
@@ -47,5 +58,37 @@ public class PassengerSeatDTO {
 
     public void setIsCheckedIn(Boolean isCheckedIn) {
         this.isCheckedIn = isCheckedIn;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getPassport() {
+        return passport;
+    }
+
+    public void setPassport(String passport) {
+        this.passport = passport;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
